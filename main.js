8f73d385836cd813b733fca6663cb3a3ad9c5b5d
jhyun94 <hyunjonathan@gmail.com>
2017-03-15 12:38:05 -0500

    got sounds to work but there is delay

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
 document.getElementsByTagName("body")[0].addEventListener("keydown", function(){
-  alert("This is working");
-})
+  var key = event.keyCode;
+  playSound(key);
+});
+
+function playSound(key){
+  switch(key){
+  case 65:
+    document.getElementById("clap").play();
+    break;
+  case 83:
+    document.getElementById("hihat").play();
+    break;
+  case 68:
+    document.getElementById("kick").play();
+    break;
+  case 70:
+    document.getElementById("openhat").play();
+    break;
+  case 71:
+    document.getElementById("boom").play();
+    break;
+  case 72:
+    document.getElementById("ride").play();
+    break;
+  case 74:
+    document.getElementById("snare").play();
+    break;
+  case 75:
+    document.getElementById("tom").play();
+    break;
+  case 76:
+    document.getElementById("tink").play();
+    break;
+  }
+}
